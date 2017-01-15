abfa5252fad43bc3a1a8b265ed7c503f9b75f883
Lars De Richter <lars@anansi.be>
2017-01-15 11:38:23 +0100

    Create my preferred working directories

--- post-install.sh
+++ post-install.sh
@@ -6,6 +6,11 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# Make some of my prefered directories
+mkdir ~/Projects
+mkdir ~/Websites
+mkdir ~/Desktop/temp
+
 # Install XCode command line tools
 xcode-select --install
 
