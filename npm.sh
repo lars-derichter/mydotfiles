36915a7af2fd7a3ba6825914e6344097b414f627
Lars De Richter <lars.derichter@gmail.com>
2017-05-26 16:31:42 +0200

    I like my properties ordered

--- npm.sh
+++ npm.sh
@@ -7,5 +7,6 @@ npm install -g grunt-cli
 npm install -g standard
 npm install -g stylelint
 npm install -g stylelint-config-standard
+npm install -g stylelint-config-idiomatic-order
 npm install -g stylefmt
 npm install -g yo
