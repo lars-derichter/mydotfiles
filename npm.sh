53c52e0e0c989b2fae8f6b9e95b80a9f14690641
Lars De Richter <lars.derichter@gmail.com>
2017-05-26 15:06:22 +0200

    Standard configuration for stylelint

--- npm.sh
+++ npm.sh
@@ -6,5 +6,6 @@ npm install -g bower
 npm install -g grunt-cli
 npm install -g standard
 npm install -g stylelint
+npm install -g stylelint-config-standard
 npm install -g stylefmt
 npm install -g yo
